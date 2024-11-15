e7f175a2abfd706cbbf426bdd23cce3bf17aaa7b
Brigham Dean <Briggs-dean@outlook.com>
2024-11-14 16:30:51 -0900

    prog1

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,13 +1,12 @@
 public class Journal
 {
     private List<Entry> _entries = new List<Entry>();
-
-    //add a new entry to the journal
+    //add a new entry 
     public void AddEntry(Entry newEntry)
     {
         _entries.Add(newEntry);
     }
-    //display all entries in the journal
+    //show journal
     public void Display()
     {
         foreach (var entry in _entries)
