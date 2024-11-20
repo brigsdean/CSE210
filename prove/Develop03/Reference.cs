f1d8be56a632b7df022cacf1720dff4c61012770
Brigham Dean <Briggs-dean@outlook.com>
2024-11-19 18:20:02 -0900

    ref

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Reference 
+{
+
+     private string _book {get; set;}
+    private int _chapter {get; set;}
+    private int _verse {get; set;}
+    private int _endVerse {get; set;}
