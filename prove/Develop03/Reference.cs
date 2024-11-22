cda0ee9707d2509cc4008dea52d649d1e4864275
Brigham Dean <Briggs-dean@outlook.com>
2024-11-21 16:15:30 -0900

    ref final

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -9,3 +9,29 @@ public class Reference
     private int _endVerse {get; set;}
 
      public Reference (string book, int chapter, int verse)
+     {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = verse;
+    }
+    public Reference (string book, int chapter, int startVerse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = startVerse;
+        _endVerse = endVerse;
+    }
+
+    public string GetDisplayText() //retorna a referencia formatada
+    {
+        if (_verse == _endVerse)
+        {
+            return $"{_book} {_chapter}:{_verse}";
+        }
+        else
+        {
+            return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        }
+    }
+}
